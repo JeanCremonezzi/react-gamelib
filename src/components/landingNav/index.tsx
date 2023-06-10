dd01a67852c9a988c4f20a890a297c5b257e8e58
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-10 19:36:01 -0300

    feat: check if cookie is set before requesting collection

--- src/components/landingNav/index.tsx
+++ src/components/landingNav/index.tsx
@@ -1,17 +1,12 @@
 import { Link } from "react-router-dom"
 import './style.css'
-import { useCookies } from "react-cookie";
 
 export const LandingNav = () => {
-    const [cookies, setCookie] = useCookies(['signin_token']);
-
     return (
         <nav className="landing-nav shadow">
             <Link to="/games">games</Link>
             <Link to="/collection">collection</Link>
-            
-            { cookies.signin_token ? <Link to="/">profile</Link> : <Link to="/">sign in</Link> }
-
+            <Link to="/">profile</Link>
         </nav>
     )
 }
--- src/pages/collection/index.tsx
+++ src/pages/collection/index.tsx
@@ -4,15 +4,19 @@ import "./style.css"
 import { useEffect, useState } from "react"
 import { AddGameInterface } from "../../services/api/interfaces.ts";
 import { GameRow } from "../../components/gameRow/index.tsx";
+import { useCookies } from "react-cookie";
 
 export const CollectionPage = () => {
     const [collection, setCollection] = useState<AddGameInterface[]>([]);
+    const [cookies, setCookie] = useCookies(['signin_token']);
 
     useEffect(() => {
-        getCollection()
-        .then((res) => {
-            setCollection(res.data);
-        })
+        if (cookies.signin_token){
+            getCollection()
+            .then((res) => {
+                setCollection(res.data);
+            })
+        }
     }, [])
     
     const handleRemoveFromCollection = (id: string) => setCollection((prevState) => prevState.filter((item) => item.gameId !== id))
